91e631fd0f773c26cf22762e0b96dfc059473957
Aashutosh Joshi <125844407+ashutoshjoshi47@users.noreply.github.com>
2026-04-10 11:41:27 +0545

    About and portfolio section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import About from "./components/About"
 import Home from "./components/Home"
 import Navbar from "./components/Navbar"
+import Portfolio from "./components/Portfolio"
+import Skills from "./components/Skills"
 import SocialLinks from "./components/SocialLinks"
 
 function App() {
@@ -11,6 +13,8 @@ function App() {
     <Navbar />
     <Home />
     <About />
+    <Portfolio />
+    <Skills />
 
     <SocialLinks />
    </div>
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div name="about" className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white'>
+
+      <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+        <div className='pb-8'>
+          <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
+        </div>
+        <p className='text-xl mt-20'>
+          I’m a Computer Engineering student with a strong passion for web development and building modern, user-friendly applications. I enjoy working with technologies like React, JavaScript, and Tailwind CSS to create responsive and interactive websites. My focus is on writing clean, efficient code and continuously improving my skills by learning new tools and best practices in development.
+        </p>
+
+        <br />
+
+        <p className='text-xl'>
+          I’m always eager to take on new challenges and turn ideas into real-world projects. As I grow in my journey, I’m looking for opportunities where I can apply my knowledge, gain hands-on experience, and contribute to meaningful projects. My goal is to become a skilled developer who can build impactful and scalable digital solutions.
+        </p>
+      </div>
+    </div>
   )
 }
 
